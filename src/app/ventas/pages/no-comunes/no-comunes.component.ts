0eea7c2f3600022d2fdb73f1bfea97dd46b24ebd
ivani2 <eltalivanitodos@gmail.com>
2022-07-12 22:41:21 -0500

    fin seccion 10 appPipes

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -18,7 +19,7 @@ export class NoComunesComponent{
     }
 
     //i18nPlural
-    clientes: string[] = ['Ivan','Van', 'Xena' , 'Xena', 'Xena', 'Xena', 'Xena', 'Xena'];
+    clientes: string[] = ['Ivan','Van', 'Xena' , 'Rosalia', 'Ri', 'Edson', 'Pele', 'Xochitl'];
 
     clientesMapa = {
       '=0': 'no tenemos ningún cliente esperando.',
@@ -41,5 +42,35 @@ export class NoComunesComponent{
     eliminarCliente(){
       this.clientes.pop();
     }
-  
+  //KeyValue Pipe
+  persona = {
+    nombre: 'Ivan',
+    edad: 27,
+    direccion: 'Morelia, Michoacán'
+  }
+  //JsonPipe
+  heroes = [
+    {
+      nombre: 'Superman',
+      vuela: true
+    },
+    {
+      nombre: 'Batman',
+      vuela: false
+    },
+    {
+      nombre: 'Aquaman',
+      vuela: false
+    },
+  ]
+
+  //Async Pipe
+  miObservable = interval( 5000 );//0, 1, 2, 3, 4
+
+  valorPromesa = new Promise( (resolve, reject) => {
+    setTimeout(() => {
+      resolve( 'Tenemos data de promesa' );
+    }, 5000);
+  } );
+
 }
